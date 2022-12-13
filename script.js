bec73cebcfc7bcba403ae1158e11bd13cf1eb04e
Mathias Bamas <mathias.bamas@etu.univ-amu.fr>
2022-12-13 17:37:15 +0100

    Suppression de la possibilité de descendre quand le menu est ouvert

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function afficheMenuDeroulant(){
     var x = document.getElementsByTagName("nav")[0]
     if(x.className === "menu-close"){
         x.className = "menu-open"
+        document.getElementsByTagName("body")[0].style.overflow="hidden"
     }
     else{
         x.className = "menu-close";
+        document.getElementsByTagName("body")[0].style.overflow="visible"
     }
 }
